38ab6a0fcbc776a72e058188fe83117a5eedd380
Ferdiansyah Pratama <Psocietyyy@gmail.com>
2025-04-26 15:12:36 +0700

    ubah beberapa kolom di users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text("bio")->nullable();
+            $table->string("avatar")->nullable();
+            $table->enum("role", ["admin", "user"])->default("user");
+            $table->boolean("is_premium")->default(false);
+            $table->date("premium_until")->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
